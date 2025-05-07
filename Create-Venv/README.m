78932e7dd55d172a85078fb2635e38c2680798d5
Giovana da Penha Rocha <88893667+giovanapr@users.noreply.github.com>
2025-05-07 19:23:19 -0300

    Create README.m

---
+++ Create-Venv/README.m
@@ -0,0 +1,7 @@
+To create a virtual environment, go to your project’s directory and run the following command. This will create a new virtual environment in a local folder named .venv:
+
+    python3 -m venv .venv
+
+Activate a virtual environment
+
+    source .venv/bin/activate
